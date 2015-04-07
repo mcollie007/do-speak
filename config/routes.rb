e51013566a6419cc482562c8233e4541548805c3
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-04-07 11:57:37 -0400

    routes cosmetic adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #    get :demo
   #  end
  # end
-  get 'help/index' => 'help#index'
+  get 'help' => 'help#index'
   root 'tasks#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
